b1961d06c9a49c72e4c60a961cddb953b5e7ef8d
Tristen <tristen.hunter.dev@gmail.com>
2026-03-10 19:23:42 +0200

    Initial commit - Spring Boot wallet API setup

---
+++ wallet-api/src/test/java/dev/hunter/tristen/wallet_api/WalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hunter.tristen.wallet_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
